61b46506cf75f992601dc7ec2e1670c5e65a8c8e
Abdulrhman Daleh <abdulrhman.daleh.dev@gmail.com>
2026-09-24 12:01:23 -0400

    fix(renewals): handle null renewId safely

--- Data Access/MembershipRenewData.cs
+++ Data Access/MembershipRenewData.cs
@@ -14,6 +14,9 @@ namespace DataAccess
         {
             MembershipRenewDTO renewDto = null;
 
+            if (!renewId.HasValue)
+                return null;
+
             using (SqlConnection connection = new SqlConnection(ConnectionAccess.GetConnectionString()))
             {
                 string query = @"SELECT RenewID,
@@ -27,7 +30,7 @@ namespace DataAccess
                 {
                     try
                     {
-                        command.Parameters.Add("@RenewID", SqlDbType.Int).Value = (object)renewId ?? DBNull.Value;
+                        command.Parameters.Add("@RenewID", SqlDbType.Int).Value = renewId.Value;
 
                         connection.Open();
 
